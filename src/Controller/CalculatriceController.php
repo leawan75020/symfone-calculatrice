db1872016dc60b0cb30d6333e660a0a4e94e6d46
lea WAN <leawan75020@gmail.com>
2023-03-16 10:15:06 +0100

    style.css

--- src/Controller/CalculatriceController.php
+++ src/Controller/CalculatriceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use index;
+use SebastianBergmann\Complexity\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +12,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CalculatriceController extends AbstractController
 {
     #[Route('/calculatrice/accueil', name: 'app_calculatrice')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        //tester la request est post  (si le formulaire est envoyé)
+        if($request->isMethod('POST')){
+            //recupere les champs du formulaires(l'opration, x et y)
+            $operation= $request->request->get('operation');
+            $x= $request->request->get('x');
+            $y= $request->request->get('y');
+
+            //si operation est une addition
+            if($operation=='addition'){
+                //rediriger vers la page d'addition
+                return $this->redirectToRoute('app_calculatrice_addition', [
+                    //speccification des 2 parametres 
+                    'x' => $x,
+                    'y' => $y,
+                ]);
+            }
+
+            //si operation est une soustraction
+            if($operation=='soustraction'){
+                //rediriger vers la page d'soustraction
+                return $this->redirectToRoute('app_calculatrice_soustraction', [
+                    //speccification des 2 parametres 
+                    'x' => $x,
+                    'y' => $y,
+                ]);
+            }
+
+            //si operation est une multiplication
+            if($operation=='multiplication'){
+                //rediriger vers la page d'multiplication
+                return $this->redirectToRoute('app_calculatrice_multiplication', [
+                    //speccification des 2 parametres 
+                    'x' => $x,
+                    'y' => $y,
+                ]);
+            }
+
+            //si operation est une division
+            if($operation=='division'){
+                //rediriger vers la page d'division
+                return $this->redirectToRoute('app_calculatrice_division', [
+                    //speccification des 2 parametres 
+                    'x' => $x,
+                    'y' => $y,
+                ]);
+            }
+
+
+        }
+
+
         return $this->render('calculatrice/index.html.twig', [
             'controller_name' => 'CalculatriceController',
+            
         ]);
     }
 
-    #[Route('/calculatrice/additionner/{x}/{y}', name: 'app_calculatrice_additionner')]
-    public function additionner(int $x, int $y): Response
+    #[Route('/calculatrice/addition/{x}/{y}', name: 'app_calculatrice_addition')]
+    public function addition(int $x, int $y): Response
     {
         //additionner x et y
         $total = $x+$y;
@@ -36,7 +89,7 @@ class CalculatriceController extends AbstractController
     #[Route('/calculatrice/soustraction/{x}/{y}', name: 'app_calculatrice_soustraction')]
     public function soustraction(int $x, int $y): Response
     {
-        //additionner x et y
+        //soustraction x et y
         $total = $x-$y;
         //affichage du twig
         return $this->render('calculatrice/soustraction.html.twig',[
@@ -47,10 +100,10 @@ class CalculatriceController extends AbstractController
         ]);
     }
 
-    #[Route('/calculatrice/multiplier/{x}/{y}', name: 'app_calculatrice_multiplier')]
-    public function multiplier(int $x, int $y): Response
+    #[Route('/calculatrice/multiplication/{x}/{y}', name: 'app_calculatrice_multiplication')]
+    public function multiplication(int $x, int $y): Response
     {
-        //additionner x et y
+        //multiplier x et y
         $total = $x*$y;
         //affichage du twig
         return $this->render('calculatrice/multiplier.html.twig',[
@@ -61,10 +114,10 @@ class CalculatriceController extends AbstractController
         ]);
     }
 
-    #[Route('/calculatrice/diviser/{x}/{y}', name: 'app_calculatrice_diviser')]
-    public function diviser(int $x, int $y): Response
+    #[Route('/calculatrice/division/{x}/{y}', name: 'app_calculatrice_division')]
+    public function division(int $x, int $y): Response
     {
-        //additionner x et y
+        //division x et y
         $total = $x/$y;
         //affichage du twig
         return $this->render('calculatrice/diviser.html.twig',[
